9c7197f5e6704332ce94619fe81ebb7b3e1e35cd
chkeethi2001 <chkeethi2001@gmail.com>
2025-09-05 17:38:53 +0530

    Update axiosInstance.js

--- job_portal/frontend/src/utils/axiosInstance.js
+++ job_portal/frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import tokenHelper from './tokenHelper'
 
-const baseURL = import.meta.env.VITE_API_URL || import.meta.env.REACT_APP_API_URL || 'http://localhost:4000/api'
+const baseURL = import.meta.env.VITE_API_URL || import.meta.env.REACT_APP_API_URL || 'https://job-portal-backend-15.onrender.com/api'
 
 const instance = axios.create({
   baseURL,
