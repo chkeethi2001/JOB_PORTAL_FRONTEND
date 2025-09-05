713e833c11274da5a9a11f8f8b9056a36e7583d3
chkeethi2001 <chkeethi2001@gmail.com>
2025-09-05 17:26:57 +0530

    Update jobsAPI.js

--- job_portal/frontend/src/api/jobsAPI.js
+++ job_portal/frontend/src/api/jobsAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/jobs`
+  baseURL: `https://job-portal-backend-15.onrender.com/jobs`
 });
 
 // Public
