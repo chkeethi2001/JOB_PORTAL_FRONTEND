9251f9486c5e63f433359ad3e6f3ca1e9871eb43
chkeethi2001 <chkeethi2001@gmail.com>
2025-09-05 17:27:50 +0530

    Update ChatBox.jsx

--- job_portal/frontend/src/components/ChatBox.jsx
+++ job_portal/frontend/src/components/ChatBox.jsx
@@ -11,7 +11,7 @@ const ChatBox = () => {
     setMessages([...messages, { sender: "user", text: input }]);
     SetIntput("");
     try{
-      const response = await axios.post(`http://localhost:5000/api/chat`,{
+      const response = await axios.post(`https://job-portal-backend-15.onrender.com/api/chat`,{
         messages:input,
         senderId:'uers',
         applicationId:'12345'
